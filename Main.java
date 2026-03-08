17adb398c779abf49ad422a130a7752e59c36bc7
Prashanthcoder <tp120113@gmail.com>
2026-03-08 13:46:42 +0530

    Dev B: Prashanth added the running feature implementation

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 public class Main {
-    public static void main(String[] args){
-        Workout w=new Workout();
-        w.name="Morning Running";
-        w.duration=15;
+    public static void main(String[] args) {
+        Running r = new Running();
+        r.name = "Morning running";
+        r.duration = 40;
+        r.distance = 5;
+        r.showworkout();
+        r.showrunning();
 
-        w.showworkout();
-        
     }
-    
+
 }
---
+++ Running.java
@@ -0,0 +1,7 @@
+public class Running extends Workout {
+    int distance;
+
+    void showrunning() {
+        System.out.println("The distance in kms : " + distance);
+    }
+}
